74ad486e1c83f58de88ca3ebbe2bc6fede9bd90a
Nahom-Derese <nahomderese@gmail.com>
2024-08-27 11:18:44 +0300

    feat: Update Loan entity with additional fields and validation
    The commit updates the Loan entity in the domain/entities directory. It adds new fields such as LoanID, Amount, InterestRate, Term, StartDate, DueDate, and Status. These fields are necessary for managing loan information in the application.

--- domain/entities/loan.go
+++ domain/entities/loan.go
@@ -1,3 +1,13 @@
 package entities
 
-type Loan struct{}
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Loan struct {
+	LoanID       primitive.ObjectID `bson:"loan_id,omitempty" json:"loanId,omitempty"`
+	Amount       float64            `bson:"amount" json:"amount" validate:"required,gt=0"`
+	InterestRate float64            `bson:"interest_rate" json:"interestRate" validate:"required,gt=0"`
+	Term         int                `bson:"term" json:"term" validate:"required,gt=0"` // Term in months
+	StartDate    int64              `bson:"start_date" json:"startDate" validate:"required"`
+	DueDate      int64              `bson:"due_date" json:"dueDate" validate:"required"`
+	Status       string             `bson:"status" json:"status" validate:"required,oneof=active closed delinquent"`
+}
