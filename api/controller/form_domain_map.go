643dd97044d103cc3294f374e787d14d490075bf
Nahom-Derese <nahomderese@gmail.com>
2024-08-27 12:04:13 +0300

    feat: Refactor form_domain_map.go to improve code structure and readability

--- api/controller/form_domain_map.go
+++ api/controller/form_domain_map.go
@@ -1,17 +1,18 @@
-package controllers
+package controller
 
 import (
+	"github.com/Nahom-Derese/Loan-Tracking-API/domain/entities"
 	"github.com/Nahom-Derese/Loan-Tracking-API/domain/forms"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func MapRegisterFormToUser(form *forms.RegisterUserForm) *entites.User {
-	return &entites.User{
+func MapRegisterFormToUser(form *forms.RegisterUserForm) *entities.User {
+	return &entities.User{
 		ID:        primitive.NewObjectID(),
 		FirstName: form.FirstName,
 		LastName:  form.LastName,
 		Email:     form.Email,
-		Password:  form.Password, // Make sure to hash the password
+		Password:  form.Password,
 		Phone:     form.Phone,
 		Address:   form.Address,
 	}
