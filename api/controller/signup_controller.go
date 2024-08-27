6d022f4d87038d182a3e82ad5943d569f59c144e
Nahom-Derese <nahomderese@gmail.com>
2024-08-27 12:04:22 +0300

    feat: Add signup controller and routes
    The commit adds the `SignupController` struct and its associated routes to handle user signup functionality. This addition is necessary for allowing users to create new accounts in the application.

---
+++ api/controller/signup_controller.go
@@ -0,0 +1,130 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	b64 "encoding/base64"
+
+	"github.com/Nahom-Derese/Loan-Tracking-API/api/middleware"
+	"github.com/Nahom-Derese/Loan-Tracking-API/bootstrap"
+	"github.com/Nahom-Derese/Loan-Tracking-API/domain/entities"
+	custom_error "github.com/Nahom-Derese/Loan-Tracking-API/domain/errors"
+	tokenutil "github.com/Nahom-Derese/Loan-Tracking-API/internal/auth"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type SignupController struct {
+	SignupUsecase entities.SignupUsecase
+	Env           *bootstrap.Env
+}
+
+func (sc *SignupController) VerifyEmail(c *gin.Context) {
+	Verificationtoken := c.Param("token")
+	decodedToken, _ := b64.URLEncoding.DecodeString(Verificationtoken)
+
+	valid, err := tokenutil.IsAuthorized(string(decodedToken), sc.Env.VerificationTokenSecret)
+
+	fmt.Println(string(decodedToken))
+
+	if !valid || err != nil {
+		c.JSON(http.StatusUnauthorized, custom_error.ErrMessage(custom_error.ErrInvalidToken))
+		return
+	}
+
+	claims, err := tokenutil.ExtractUserClaimsFromToken(string(decodedToken), sc.Env.VerificationTokenSecret)
+	userID := claims["id"].(string)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+		return
+	}
+	user, err := sc.SignupUsecase.GetUserById(context.TODO(), userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+		return
+	}
+	if user.Active {
+		c.JSON(http.StatusConflict, custom_error.ErrMessage(custom_error.ErrAlreadyVerified))
+		return
+	}
+
+	err = sc.SignupUsecase.ActivateUser(context.TODO(), userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Email verified successfully"})
+
+}
+func (sc *SignupController) Signup(c *gin.Context) {
+	var request entities.SignupRequest
+
+	err := c.ShouldBindJSON(&request)
+	if err != nil {
+		if err == io.EOF {
+			c.JSON(http.StatusBadRequest, custom_error.ErrMessage(custom_error.EreInvalidRequestBody))
+			return
+		}
+		middleware.CustomErrorResponse(c, err)
+		return
+	}
+
+	_, err = sc.SignupUsecase.GetUserByEmail(c, request.Email)
+	if err == nil {
+		c.JSON(http.StatusConflict, custom_error.ErrMessage(custom_error.ErrUserAlreadyExists))
+		return
+	}
+
+	encryptedPassword, err := bcrypt.GenerateFromPassword(
+		[]byte(request.Password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+		return
+	}
+
+	request.Password = string(encryptedPassword)
+
+	var role string
+
+	user := entities.User{
+		ID:        primitive.NewObjectID(),
+		FirstName: request.FirstName,
+		LastName:  request.LastName,
+		Email:     request.Email,
+		Password:  request.Password,
+		Active:    false,
+		Role:      role,
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+	}
+
+	VerificationToken, err := sc.SignupUsecase.CreateVerificationToken(&user, sc.Env.VerificationTokenSecret, sc.Env.VerificationTokenExpiryMin)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+		return
+	}
+
+	_, err = sc.SignupUsecase.Create(c, &user)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+		return
+	}
+
+	//send email
+	encodedToken := b64.URLEncoding.EncodeToString([]byte(VerificationToken))
+	err = sc.SignupUsecase.SendVerificationEmail(user.Email, encodedToken, sc.Env)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, custom_error.ErrMessage(err))
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "email sent successfully, please verify your email"})
+
+}
