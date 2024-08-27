90c9092f8628aaf33e8b589d85298ce1e4e8e843
Nahom-Derese <nahomderese@gmail.com>
2024-08-27 16:55:30 +0300

    Refactor auth middleware to remove unnecessary user owner claim

--- domain/entities/login.go
+++ domain/entities/login.go
@@ -2,10 +2,6 @@ package entities
 
 import "context"
 
-type LoginRequest struct {
-	Email    string `json:"email" bson:"email" binding:"required,email"`
-	Password string `json:"password" bson:"password" binding:"required,min=4,max=30"`
-}
 type LoginResponse struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
--- usecase/signup_usecase.go
+++ usecase/signup_usecase.go
@@ -23,14 +23,18 @@ func NewSignupUsecase(userRepository entities.UserRepository, timeout time.Durat
 }
 
 func (su *signupUsecase) GetUserById(c context.Context, userId string) (*entities.User, error) {
-	user, err := su.userRepository.GetUserById(c, userId)
+	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+	defer cancel()
+	user, err := su.userRepository.GetUserById(ctx, userId)
 	return user, err
 }
 func (su *signupUsecase) ActivateUser(c context.Context, userID string) error {
-	_, err := su.userRepository.ActivateUser(c, userID)
-	return err
+	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+	defer cancel()
+	return su.userRepository.ActivateUser(ctx, userID)
 }
 func (su *signupUsecase) Create(c context.Context, user *entities.User) (*entities.User, error) {
+
 	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
 	defer cancel()
 	return su.userRepository.CreateUser(ctx, user)
@@ -47,17 +51,21 @@ func (su *signupUsecase) GetUserByEmail(c context.Context, email string) (entiti
 }
 
 func (su *signupUsecase) CreateAccessToken(user *entities.User, secret string, expiry int) (accessToken string, err error) {
+
 	return tokenutil.CreateAccessToken(user, secret, expiry)
 }
 
 func (su *signupUsecase) CreateRefreshToken(user *entities.User, secret string, expiry int) (refreshToken string, err error) {
+
 	return tokenutil.CreateRefreshToken(user, secret, expiry)
 }
 
 func (su *signupUsecase) CreateVerificationToken(user *entities.User, secret string, expiry int) (refreshToken string, err error) {
+
 	return tokenutil.CreateVerificationToken(user, secret, expiry)
 }
 
 func (su *signupUsecase) SendVerificationEmail(recipientEmail string, encodedToken string, env *bootstrap.Env) (err error) {
+
 	return emailutil.SendVerificationEmail(recipientEmail, encodedToken, env)
 }
